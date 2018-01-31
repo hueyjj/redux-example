debace27a01675a09b3bd13834674b543a5480aa
huey <Huey Freeman>
2018-01-30 23:43:52 -0800

    inital commit

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+
+import configureStore from './store/configureStore';
+import RootContainer from "./containers/RootContainer";
+import Root from './components/Root';
+
+let store = configureStore();
+ReactDOM.render(
+  <Provider store={store}>
+    <RootContainer />
+  </Provider>
+  , document.getElementById('root')
+);
